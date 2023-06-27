5122ed72fd7bf4f05b9fb650dbc2aa0d066a5d59
Booster254 <williamnyakeri254@gmail.com>
2023-06-27 08:00:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,77 +1,99 @@
 const form = document.getElementById('taxCalculatorForm');
 const calculateBtn = document.getElementById('calculateBtn');
 const resultsContainer = document.getElementById('results');
+let periodType; 
 
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   calculateResults();
 });
 
-// taxes and deductions calculations
+// Taxes and deductions
 function calculateIncomeTax(annualIncome) {
+  let tax = 0;
+  const personalRelief = 28800;
+
   if (annualIncome <= 147580) {
-    return annualIncome * 0.1;
+    tax = annualIncome * 0.1;
   } else if (annualIncome <= 286623) {
-    return (annualIncome - 147580) * 0.15 + 14758;
+    tax = (annualIncome - 147580) * 0.15 + 14758;
   } else if (annualIncome <= 425666) {
-    return (annualIncome - 286623) * 0.2 + 39303;
+    tax = (annualIncome - 286623) * 0.2 + 39303;
   } else if (annualIncome <= 564709) {
-    return (annualIncome - 425666) * 0.25 + 63653;
+    tax = (annualIncome - 425666) * 0.25 + 63653;
   } else {
-    return (annualIncome - 564709) * 0.3 + 110298;
+    tax = (annualIncome - 564709) * 0.3 + 110298;
   }
+
+  let paye;
+  if (periodType === 'month') {
+    paye = Math.max((tax - personalRelief) / 12, 0);
+  } else {
+    paye = Math.max(tax - personalRelief, 0);
+  }
+
+  return paye;
 }
 
 function calculateNHIFContribution(annualIncome) {
-  if (annualIncome <= 5999) {
-    return 150;
-  } else if (annualIncome <= 7999) {
-    return 300;
-  } else if (annualIncome <= 11999) {
-    return 400;
-  } else if (annualIncome <= 14999) {
-    return 500;
-  } else if (annualIncome <= 19999) {
-    return 600;
-  } else if (annualIncome <= 24999) {
-    return 750;
-  } else if (annualIncome <= 29999) {
-    return 850;
-  } else if (annualIncome <= 34999) {
-    return 900;
-  } else if (annualIncome <= 39999) {
-    return 950;
-  } else if (annualIncome <= 44999) {
-    return 1000;
-  } else if (annualIncome <= 49999) {
-    return 1100;
-  } else if (annualIncome <= 59999) {
-    return 1200;
-  } else if (annualIncome <= 69999) {
-    return 1300;
-  } else if (annualIncome <= 79999) {
-    return 1400;
-  } else if (annualIncome <= 89999) {
-    return 1500;
-  } else if (annualIncome <= 99999) {
-    return 1600;
-  } else {
-    return 1700;
+  const nhifRates = [
+    { minIncome: 0, maxIncome: 5999, contribution: 150 },
+    { minIncome: 6000, maxIncome: 7999, contribution: 300 },
+    { minIncome: 8000, maxIncome: 11999, contribution: 400 },
+    { minIncome: 12000, maxIncome: 14999, contribution: 500 },
+    { minIncome: 15000, maxIncome: 19999, contribution: 600 },
+    { minIncome: 20000, maxIncome: 24999, contribution: 750 },
+    { minIncome: 25000, maxIncome: 29999, contribution: 850 },
+    { minIncome: 30000, maxIncome: 34999, contribution: 900 },
+    { minIncome: 35000, maxIncome: 39999, contribution: 950 },
+    { minIncome: 40000, maxIncome: 44999, contribution: 1000 },
+    { minIncome: 45000, maxIncome: 49999, contribution: 1100 },
+    { minIncome: 50000, maxIncome: 59999, contribution: 1200 },
+    { minIncome: 60000, maxIncome: 69999, contribution: 1300 },
+    { minIncome: 70000, maxIncome: 79999, contribution: 1400 },
+    { minIncome: 80000, maxIncome: 89999, contribution: 1500 },
+    { minIncome: 90000, maxIncome: 99999, contribution: 1600 },
+    { minIncome: 100000, maxIncome: Infinity, contribution: 1700 },
+    { minIncome: 0, maxIncome: Infinity, contribution: 500 }, // Self-employed contribution
+  ];
+
+  let contribution = 0;
+  for (let i = 0; i < nhifRates.length; i++) {
+    const rate = nhifRates[i];
+    if (annualIncome >= rate.minIncome && annualIncome <= rate.maxIncome) {
+      contribution = rate.contribution;
+      break;
+    }
   }
+
+  return contribution;
 }
 
 function calculateNSSFContribution(annualIncome) {
-  if (annualIncome > 18000) {
-    return 2160;
-  } else {
-    return annualIncome * 0.12;
+  const nssfRates = {
+    old: 200,
+    new: 0.12, // 12% of pensionable wages
+  };
+
+  const lowerEarningLimit = 6000;
+  const upperEarningLimit = 18000;
+
+  let contribution = 0;
+  if (annualIncome > lowerEarningLimit) {
+    const pensionableWages = Math.min(annualIncome, upperEarningLimit);
+    contribution = pensionableWages * nssfRates.new;
+    if (annualIncome > upperEarningLimit) {
+      contribution += nssfRates.old;
+    }
   }
+
+  return contribution;
 }
 
-// Calculate results
+// results calculations
 function calculateResults() {
-  // Get form values
-  const periodType = document.querySelector('input[name="periodType"]:checked').value;
+//values
+  periodType = document.querySelector('input[name="periodType"]:checked').value;
   const basicSalary = parseFloat(document.getElementById('basicSalary').value);
   const benefits = parseFloat(document.getElementById('benefits').value);
   const deductNSSF = document.querySelector('input[name="deductNSSF"]:checked').value;
@@ -86,16 +108,17 @@ function calculateResults() {
   const nhifContribution = deductNHIF === 'yes' ? calculateNHIFContribution(annualIncome) : 0;
 
   // NSSF contribution
-  const nssfContribution = deductNSSF === 'yes' ? (nssfRates === 'new' ? calculateNSSFContribution(annualIncome) : 200) : 0;
+  const nssfContribution =
+    deductNSSF === 'yes' ? (nssfRates === 'new' ? calculateNSSFContribution(annualIncome) : 200) : 0;
 
-  // Determine personal relief based on period type
+  // Personal relief
   const personalRelief = periodType === 'month' ? 2700 : 28800;
 
   let netSalary;
   if (periodType === 'month') {
-    netSalary = (basicSalary + benefits) - (incomeTax / 12) - (nhifContribution / 12) - (nssfContribution / 12);
+    netSalary = basicSalary + benefits - incomeTax / 12 - nhifContribution / 12 - nssfContribution / 12;
   } else {
-    netSalary = (basicSalary + benefits) - incomeTax - nhifContribution - nssfContribution;
+    netSalary = basicSalary + benefits - incomeTax - nhifContribution - nssfContribution;
   }
 
   // Display results
